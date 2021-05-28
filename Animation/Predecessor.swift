5ed8213048cf4523358e0110077c02cb21b22f97
Nicole Liu <nicoleliu_@outlook.com>
2021-05-28 14:15:23 -0400

    Added leaf system code

--- Animation/Predecessor.swift
+++ Animation/Predecessor.swift
@@ -1,11 +1,11 @@
-////
-////  Predecessor.swift
-////  Animation
-////
-////  Created by Russell Gordon on 2021-05-05.
-////
 //
-//import Foundation
+//  Predecessor.swift
+//  Animation
 //
-//// Make code in the L-System more readable...
-//typealias Predecessor = String
+//  Created by Russell Gordon on 2021-05-05.
+//
+
+import Foundation
+
+// Make code in the L-System more readable...
+typealias Predecessor = String
